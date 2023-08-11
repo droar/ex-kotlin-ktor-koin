8964779a2cb20a5d62b126d3675dc291ac43c806
droar <sergio.roa@outlook.com>
2023-08-11 14:03:44 +0200

    Init examples

---
+++ src/main/kotlin/com/droar/samples/Application.kt
@@ -0,0 +1,14 @@
+package com.droar.samples
+
+import com.droar.samples.plugins.*
+import io.ktor.server.application.*
+
+fun main(args: Array<String>) {
+    io.ktor.server.netty.EngineMain.main(args)
+}
+
+fun Application.module() {
+    configureSerialization()
+    configureDatabases()
+    configureRouting()
+}
